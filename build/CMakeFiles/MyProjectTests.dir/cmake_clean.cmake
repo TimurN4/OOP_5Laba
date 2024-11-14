1cd4b403774d9d8732d6ecf63f876b1dcbe930e4
TimurN4 <nogaevtimur@gmail.com>
2024-11-14 23:56:32 +0300

    Full laba 5 with gtests

---
+++ build/CMakeFiles/MyProjectTests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProjectTests.dir/test/test.cpp.o"
+  "CMakeFiles/MyProjectTests.dir/test/test.cpp.o.d"
+  "MyProjectTests"
+  "MyProjectTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProjectTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/timurnogaev/unix-shell-and-utils-TimurN4/OOP_5Laba
+# Build directory: /home/timurnogaev/unix-shell-and-utils-TimurN4/OOP_5Laba/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(MyProjectTests "/home/timurnogaev/unix-shell-and-utils-TimurN4/OOP_5Laba/build/MyProjectTests")
+set_tests_properties(MyProjectTests PROPERTIES  _BACKTRACE_TRIPLES "/home/timurnogaev/unix-shell-and-utils-TimurN4/OOP_5Laba/CMakeLists.txt;59;add_test;/home/timurnogaev/unix-shell-and-utils-TimurN4/OOP_5Laba/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
